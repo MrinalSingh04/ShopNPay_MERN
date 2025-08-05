dc72f3ac48716e38a141fee69a7b9a067cdb5739
Mrinal Singh <mrinalsingh7000@gmail.com>
2025-08-05 22:06:28 +0530

    routes-setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,20 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+
 dotenv.config({ path: "backend/config/config.env" });
+
+app.get("/api/v1/products", (req, res) => {
+  res.status(200).json({
+    message: "All products fetched successfully",
+    success: true,
+  });
+});
+app.get("/api/v1/product", (req, res) => {
+  res.status(200).json({
+    message: "Single product fetched successfully",
+    success: true,
+  });
+});
 
 app.listen(process.env.PORT || 8000, () => {
   console.log(`Server is running on port ${process.env.PORT || 3000}`);
